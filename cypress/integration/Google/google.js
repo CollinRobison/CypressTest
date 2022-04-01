eeeadb3f7089236ffcbc6a9f8ba2f1df292c75da
Collin Robison <collinrobison@gmail.com>
2022-04-01 00:35:50 -0500

    added a pageobject

--- cypress/integration/Google/google.js
+++ cypress/integration/Google/google.js
@@ -1,14 +1,18 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import googleHomepage from '../../support/pageObjects/googleHomepage';
 
-const url = 'https://google.com'
+const url = 'https://google.com';
+const GoogleHomepage = new googleHomepage();
+
+
 Given('I visit the Google homepage', () => {
   cy.visit(url)
 })
 
 When('I type "{}" in the search bar', (SearchValue) => {
-    cy.get('input[title="Search"]').type(SearchValue);
+  GoogleHomepage.getSearchBar().type(SearchValue);
 })
 
 Then('"{}" appears in the search bar', (SearchValue) => {
-    cy.get('input[title="Search"]').should('have.value', SearchValue)
+  GoogleHomepage.getSearchBar().should('have.value', SearchValue)
 })
---
+++ cypress/support/pageObjects/googleHomepage.js
@@ -0,0 +1,8 @@
+class googleHomepage {
+    
+getSearchBar() {
+        return cy.get('[title="Search"]');
+}
+}
+
+export default googleHomepage
